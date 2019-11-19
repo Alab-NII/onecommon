c55c438873d7f77b4d813c2d5a2bc6d9af932107
Takuma Udagawa <takuma.udagawa1234@gmail.com>
2019-11-19 16:53:46 +0900

    remove unnecessary comments

--- aaai2020/experiments/models/ctx_encoder.py
+++ aaai2020/experiments/models/ctx_encoder.py
@@ -14,85 +14,6 @@ import torch.nn.functional as F
 
 from models.utils import *
 
-
-class AttributeContextEncoder(nn.Module):
-    def __init__(self, domain, args):
-        super(AttributeContextEncoder, self).__init__()
-
-        self.num_ent = domain.num_ent()
-        self.dim_ent = domain.dim_ent()
-
-        assert args.nembed_ctx % self.dim_ent == 0
-
-        self.hidden_size = int(args.nembed_ctx / self.dim_ent)
-        #self.hidden_size = int(args.nembed_ctx / 2)
-
-        self.x_value_encoder = nn.Sequential(
-            torch.nn.Linear(domain.num_ent(), self.hidden_size),
-            nn.Tanh(),
-            nn.Dropout(args.dropout),
-            torch.nn.Linear(self.hidden_size, self.hidden_size),
-            nn.Tanh(),
-            nn.Dropout(args.dropout)
-        )
-
-        self.y_value_encoder = nn.Sequential(
-            torch.nn.Linear(domain.num_ent(), self.hidden_size),
-            nn.Tanh(),
-            nn.Dropout(args.dropout),
-            torch.nn.Linear(self.hidden_size, self.hidden_size),
-            nn.Tanh(),
-            nn.Dropout(args.dropout)
-        )
-
-        self.size_encoder = nn.Sequential(
-            torch.nn.Linear(domain.num_ent(), self.hidden_size),
-            nn.Tanh(),
-            nn.Dropout(args.dropout),
-            torch.nn.Linear(self.hidden_size, self.hidden_size),
-            nn.Tanh(),
-            nn.Dropout(args.dropout)
-        )
-
-        self.color_encoder = nn.Sequential(
-            torch.nn.Linear(domain.num_ent(), self.hidden_size),
-            nn.Tanh(),
-            nn.Dropout(args.dropout),
-            torch.nn.Linear(self.hidden_size, self.hidden_size),
-            nn.Tanh(),
-            nn.Dropout(args.dropout)
-        )
-
-        init_cont([self.color_encoder, self.size_encoder, self.x_value_encoder, self.y_value_encoder], args.init_range)
-
-    def forward(self, ctx):
-        ctx_t = ctx.transpose(0, 1)
-        ents = ctx_t.view(ctx_t.size(0), self.num_ent, self.dim_ent, 1)
-
-        all_relative_ents = []
-        for i in range(self.num_ent):
-            relative_ents = []
-            for j in range(self.num_ent):
-                if i == j:
-                    relative_ent = ents[:,i,:,:]
-                else:
-                    relative_ent = ents[:,i,:,:] - ents[:,j,:,:]
-                relative_ents.append(relative_ent.unsqueeze(1))
-            all_relative_ents.append(torch.cat(relative_ents, 3))
-        all_relative_ents = torch.cat(all_relative_ents, 1)
-
-        x_values = self.x_value_encoder(all_relative_ents[:,:,0,:])
-        y_values = self.y_value_encoder(all_relative_ents[:,:,1,:])
-        #sizes =  self.size_encoder(all_relative_ents[:,:,2,:])
-        #colors = self.color_encoder(all_relative_ents[:,:,3,:])
-
-        #out = torch.cat([x_values, y_values, sizes, colors], 2)
-        out = torch.cat([x_values, y_values], 2)
-        #out = torch.cat([sizes, colors], 2)
-
-        return out
-
-
 class AttentionContextEncoder(nn.Module):
     def __init__(self, domain, args):
         super(AttentionContextEncoder, self).__init__()
@@ -101,31 +22,18 @@ class AttentionContextEncoder(nn.Module):
         self.dim_ent = domain.dim_ent()
 
         self.property_encoder = nn.Sequential(
-            #nn.Dropout(args.dropout),
-            #torch.nn.Linear(domain.dim_ent(), args.nembed_ctx),
             torch.nn.Linear(domain.dim_ent(), int(args.nembed_ctx / 2)),
-            #torch.nn.Linear(domain.dim_ent(), int(args.nembed_prop)),
             nn.Tanh(),
-            #nn.ReLU(),
             nn.Dropout(args.dropout)
         )
 
         self.relation_encoder = nn.Sequential(
-            #nn.Dropout(args.dropout),
-            #torch.nn.Linear(2 * domain.dim_ent(), args.nembed_ctx),
-            #torch.nn.Linear(2 * domain.dim_ent(), int(args.nembed_rel)),
-            #torch.nn.Linear(domain.dim_ent(), int(args.nembed_rel)),
-            #torch.nn.Linear(domain.dim_ent() + 1, int(args.nembed_rel)),
             torch.nn.Linear(domain.dim_ent() + 1, int(args.nembed_ctx / 2)),
             nn.Tanh(),
-            #nn.ReLU(),
             nn.Dropout(args.dropout)
         )
 
         self.dropout = nn.Dropout(args.dropout)
-        #self.fc1 = nn.Linear(args.nembed_ctx, args.nembed_ctx) 
-        #self.tanh = nn.Tanh()
-        #self.relu = nn.ReLU()
 
         init_cont([self.property_encoder, self.relation_encoder], args.init_range)
 
@@ -139,27 +47,13 @@ class AttentionContextEncoder(nn.Module):
             for j in range(self.num_ent):
                 if i == j:
                     continue
-                #rel_pairs.append(torch.cat([ents[:,i,:],ents[:,j,:]], 1).unsqueeze(1))
-                #rel_pairs.append((ents[:,i,:] - ents[:,j,:]).unsqueeze(1))
                 dist = torch.sqrt((ents[:,i,0] - ents[:,j,0])**2 + (ents[:,i,1] - ents[:,j,1])**2)
                 rel_pairs.append((torch.cat([ents[:,i,:] - ents[:,j,:], dist.unsqueeze(1)], 1).unsqueeze(1)))
-                #rel_pairs.append((torch.cat([ents[:,i,:] - ents[:,j,:], (ents[:,i,:] - ents[:,j,:])**2], 1).unsqueeze(1)))
             ent_rel_pairs.append(torch.cat(rel_pairs, 1).unsqueeze(1))
         ent_rel_pairs = torch.cat(ent_rel_pairs, 1)
         rel_emb = self.relation_encoder(ent_rel_pairs).sum(2)
-        #rel_emb = self.relation_encoder(ent_rel_pairs).max(2)[0]
-        #rel_emb = self.relation_encoder(ent_rel_pairs)
-        #rel_emb = rel_emb.view(rel_emb.size(0), rel_emb.size(1), -1)
-        #rel_emb = ent_rel_pairs.view(ent_rel_pairs.size(0), ent_rel_pairs.size(1), -1)
         out = torch.cat([prop_emb, rel_emb], 2)
-        #out = torch.add(prop_emb, rel_emb)
-        #out = self.fc1(out)
-        #out = self.tanh(out)
-        #out = self.relu(out)
-        #out = self.dropout(out)
         return out
-
-
 
 class RelationContextEncoder(nn.Module):
     def __init__(self, domain, args):
